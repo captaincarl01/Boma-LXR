04dbdde48982f9a519116da0d701f9d8f1cec312
Adeyeye Stephen <adeyeyestephen278@gmail.com>
2026-03-10 08:58:08 +0100

    first commit of file

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+//* HERO SLIDER */
+
+let slides = document.querySelectorAll(".hero-slider img");
+let index = 0;
+
+setInterval(()=>{
+
+slides[index].classList.remove("active");
+
+index = (index + 1) % slides.length;
+
+slides[index].classList.add("active");
+
+},5000);
+
+
 /* COUNTDOWN */
 
-const dropDate = new Date("April 20, 2026 00:00:00").getTime();
+const drop = new Date("April 5, 2026 00:00:00").getTime();
 
-const timer = setInterval(function(){
+setInterval(()=>{
 
 const now = new Date().getTime();
 
-const distance = dropDate - now;
+const distance = drop - now;
 
 const days = Math.floor(distance / (1000*60*60*24));
-const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
-const minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
 
 document.getElementById("countdown").innerHTML =
-days + " Days : " + hours + " Hours : " + minutes + " Minutes ";
+days + " Days : " + hours + " Hours : " + minutes + " Minutes";
 
 },1000);
